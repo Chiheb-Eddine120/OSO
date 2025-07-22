644c97a81f4dd3db6bfb3fa772fa7d641892ccd5
Chiheb-EddineM <chihebedine.mosbah@gmail.com>
2025-07-22 16:02:37 +0200

    xwsdev

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'oso-pink': '#FF466E',
+        'oso-orange': '#FF963C',
+        'oso-light-orange': '#FFC864',
+        'oso-red-orange': '#FF6450',
+        'oso-black': '#1A1A1A',
+        'oso-gray': '#6B7280',
+        'oso-light-gray': '#F8FAFC',
+        'oso-dark-gray': '#374151',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
